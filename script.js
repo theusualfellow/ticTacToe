fcf7c3974c262b7f3370801c4ef2c7d2ffa7d227
sandeep <sandeepsingh23ok@gmail.com>
2024-05-09 21:40:59 -0300

    Make X and O placement work using objects

--- script.js
+++ script.js
@@ -4,18 +4,28 @@
     ['-','-','-']
 ]
 
-function player(name){
+function player(name1){
+
+    const playerName = name1
+
     const rowPrompt = ()=>{
-        let row = prompt(`${name} choose your row`)
+        let row = prompt(`${name1} choose your row`)
         return row
     }
     const colPrompt= ()=>{
-        let column = prompt(`${name} choose your column`)
+        let column = prompt(`${name1} choose your column`)
         return column
     }
     const playerMove = function(row,column){
+        console.log(row)
         if(board[column-1][row-1]  == '-'){
-            board[column-1][row-1]  = "X"
+            if(playerName=="sandeep"){
+                board[column-1][row-1]  = "X"
+            }
+            else{
+                board[column-1][row-1]  = "O"
+
+            }
             return board
         }
         return "error"
@@ -26,11 +36,11 @@ function player(name){
 const player1 = player("sandeep")
 const row1 = player1.rowPrompt()
 const col1 = player1.colPrompt()
-player1.playerMove(row1, col1)
+console.log(player1.playerMove(row1, col1))
 const player2= player("harman")
 const row2 = player2.rowPrompt()
 const col2 = player2.colPrompt()
-player2.playerMove(row2,col2)
+console.log(player2.playerMove(row2,col2))
   
 
 
