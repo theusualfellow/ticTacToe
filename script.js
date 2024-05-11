86603ce224ff26b87cc322f38197cf32920451b8
sandeep <sandeepsingh23ok@gmail.com>
2024-05-10 21:40:57 -0300

    Make sure the row and col is between 0 and 3

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ function player(name1){
     const playerName = name1
 
     const rowPrompt = ()=>{
-        let row = prompt(`${name1} choose your row`)
+        let row
+        do{row = prompt(`${name1} choose your row`)}
+        while (row<0 || row>3)
+        
         return row
     }
     const colPrompt= ()=>{
-        let column = prompt(`${name1} choose your column`)
+        let column
+        do{column = prompt(`${name1} choose your column`)}
+        while (column<0 || row>3)
         return column
     }
     const playerMove = function(row,column){
@@ -33,15 +38,20 @@ function player(name1){
     return{rowPrompt, colPrompt, playerMove}
     }
 
-const player1 = player("sandeep")
-const row1 = player1.rowPrompt()
-const col1 = player1.colPrompt()
-console.log(player1.playerMove(row1, col1))
-const player2= player("harman")
-const row2 = player2.rowPrompt()
-const col2 = player2.colPrompt()
-console.log(player2.playerMove(row2,col2))
-  
+function playGame(){
+    const player1 = player("sandeep")
+    const row1 = player1.rowPrompt()
+    const col1 = player1.colPrompt()
+    console.log(player1.playerMove(row1, col1))
+    const player2= player("harman")
+    const row2 = player2.rowPrompt()
+    const col2 = player2.colPrompt()
+    console.log(player2.playerMove(row2,col2))
+    
+}
+function checkWinner(){
+    
+}
 
 
 
