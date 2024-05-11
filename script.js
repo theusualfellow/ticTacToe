029134b3ec602786d44dd14e71494abaaa9a4dc1
sandeep <sandeepsingh23ok@gmail.com>
2024-05-10 21:59:01 -0300

    Check for winner on very basic level

--- script.js
+++ script.js
@@ -12,23 +12,23 @@ function player(name1){
         let row
         do{row = prompt(`${name1} choose your row`)}
         while (row<0 || row>3)
-        
+        console.log(row)
         return row
     }
     const colPrompt= ()=>{
         let column
         do{column = prompt(`${name1} choose your column`)}
-        while (column<0 || row>3)
+        while (column<0 || column>3)
         return column
     }
     const playerMove = function(row,column){
         console.log(row)
-        if(board[column-1][row-1]  == '-'){
+        if(board[row-1][column-1]  == '-'){
             if(playerName=="sandeep"){
-                board[column-1][row-1]  = "X"
+                board[row-1][column-1]  = "X"
             }
             else{
-                board[column-1][row-1]  = "O"
+                board[row-1][column-1]  = "O"
 
             }
             return board
@@ -39,19 +39,31 @@ function player(name1){
     }
 
 function playGame(){
-    const player1 = player("sandeep")
-    const row1 = player1.rowPrompt()
-    const col1 = player1.colPrompt()
-    console.log(player1.playerMove(row1, col1))
-    const player2= player("harman")
-    const row2 = player2.rowPrompt()
-    const col2 = player2.colPrompt()
-    console.log(player2.playerMove(row2,col2))
+    do{
+        const player1 = player("sandeep")
+        const row1 = player1.rowPrompt()
+        const col1 = player1.colPrompt()
+        console.log(player1.playerMove(row1, col1))
+        const player2= player("harman")
+        const row2 = player2.rowPrompt()
+        const col2 = player2.colPrompt()
+        console.log(player2.playerMove(row2,col2))
+    }
+    while(checkWinner()==false)
+
+    return `winner is player 1`
+    
     
 }
 function checkWinner(){
-    
+    if(
+        board[0][0]=='X' && board[0][1]=='X' && board[0][2] == 'X'
+    ){
+        return true
+    }
+
+    return false
 }
 
 
-
+console.log(playGame())
